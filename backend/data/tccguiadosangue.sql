bb2d8f3b62085347dbfb245edb04d64760838768
dellPhonk <cmcalapica@gmail.com>
2025-11-17 18:33:18 -0300

    d

--- backend/data/tccguiadosangue.sql
+++ backend/data/tccguiadosangue.sql
@@ -1,33 +1,96 @@
 CREATE DATABASE TCCGUIADOSANGUE1;
 USE TCCGUIADOSANGUE1;
 
-CREATE TABLE tb_users (id int primary key auto_increment,
-nome varchar(222),
-email varchar(222),
-senha varchar(222),
-cpf int,
-rg int,
-telefone int,
-data_nascimento date,
-nm_mae varchar (222),
-endereco varchar(222),
-sexo varchar(222),
-tp_sanguineo varchar(222)
+
+CREATE TABLE tb_users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(222),
+    email VARCHAR(222) UNIQUE,
+    senha VARCHAR(222),
+    cpf VARCHAR(20),
+    rg VARCHAR(20),
+    telefone VARCHAR(20),
+    data_nascimento DATE,
+    nm_mae VARCHAR(222),
+    endereco VARCHAR(222),
+    sexo VARCHAR(50),
+    tp_sanguineo VARCHAR(10)
 );
 
-CREATE TABLE tb_agendamento (id int primary key auto_increment,
-pergunta1 boolean,
-pergunta2 boolean,
-pergunta2 boolean,
-pergunta2 boolean,
-pergunta2 boolean,
-pergunta2 boolean,
-pergunta2 boolean,
-pergunta2 boolean,
-pergunta2 boolean,
-pergunta2 boolean,
-pergunta2 boolean,
-pergunta2 boolean,
-dt_doacao datetime,
-local_doacao varchar (222)
+
+CREATE TABLE tb_agendamento (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_user INT
+    NOT NULL,
+
+    pergunta1 BOOLEAN,
+    pergunta2 BOOLEAN,
+    pergunta3 BOOLEAN,
+    pergunta4 BOOLEAN,
+    pergunta5 BOOLEAN,
+    pergunta6 BOOLEAN,
+    pergunta7 BOOLEAN,
+    pergunta8 BOOLEAN,
+    pergunta9 BOOLEAN,
+    pergunta10 BOOLEAN,
+    pergunta11 BOOLEAN,
+    pergunta12 BOOLEAN,
+    pergunta13 BOOLEAN,
+
+    dt_doacao DATETIME,
+    local_doacao VARCHAR(222),
+
+    CONSTRAINT fk_agendamento_user
+        FOREIGN KEY (id_user)
+        REFERENCES tb_users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+CREATE DATABASE TCCGUIADOSANGUE1;
+USE TCCGUIADOSANGUE1;
+
+
+CREATE TABLE tb_users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(222),
+    email VARCHAR(222) UNIQUE,
+    senha VARCHAR(222),
+    cpf VARCHAR(20),
+    rg VARCHAR(20),
+    telefone VARCHAR(20),
+    data_nascimento DATE,
+    nm_mae VARCHAR(222),
+    endereco VARCHAR(222),
+    sexo VARCHAR(50),
+    tp_sanguineo VARCHAR(10)
+);
+
+
+CREATE TABLE tb_agendamento (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_user INT
+    NOT NULL,
+
+    pergunta1 BOOLEAN,
+    pergunta2 BOOLEAN,
+    pergunta3 BOOLEAN,
+    pergunta4 BOOLEAN,
+    pergunta5 BOOLEAN,
+    pergunta6 BOOLEAN,
+    pergunta7 BOOLEAN,
+    pergunta8 BOOLEAN,
+    pergunta9 BOOLEAN,
+    pergunta10 BOOLEAN,
+    pergunta11 BOOLEAN,
+    pergunta12 BOOLEAN,
+    pergunta13 BOOLEAN,
+
+    dt_doacao DATETIME,
+    local_doacao VARCHAR(222),
+
+    CONSTRAINT fk_agendamento_user
+        FOREIGN KEY (id_user)
+        REFERENCES tb_users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
